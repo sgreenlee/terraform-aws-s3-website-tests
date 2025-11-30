6a03ed5461e3b8df2afaff7b7b8d9d6287e2473c
Sam Greenlee <sam.a.greenlee@gmail.com>
2025-11-30 14:46:32 -0500

    Add website_is_running test

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = "us-east-2"
+
+  profile = "admin"
+  assume_role {
+    role_arn = "arn:aws:iam::852140462465:role/OrganizationAccountAccessRole"
+  }
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
